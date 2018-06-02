3a96d49535e0336b71e24d748d84c99a2dafd319
DESKTOP-531FTF3\44yos <44yoshi02@gmail.com>
2018-06-02 13:24:03 +0900

    java_メソッド戻り値引数、戻り値1の課題を提出します

---
+++ CampChallenge_java/011.メソッド/challenge11_6.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 44yos
+ */
+@WebServlet(name = "challenge11_6", urlPatterns = {"/challenge11_6"})
+public class challenge11_6 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    String[] toArray(String id){
+        //data[n][0]=id,data[n][1]=名前,data[n][2]=生年月日,data[n][3]=住所
+        String[][] data={{"1","吉松","0414","吉井"},
+                         {"2","柴田","0829","小郡"},
+                         {"3","久保山","0121","鳥栖"}};
+        int i;
+        for(i=0;i<3;i++){
+            if(id.equals(data[i][0])){
+                int m=i;
+                break;
+            }
+        }
+        return data[i];
+    }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String data[];
+        for(int i=1;i<=3;i++){
+            String s=String.valueOf(i);
+            data=toArray(s);
+            out.println(i+"人目<br>");
+            out.println("名前："+data[1]+"<br>");
+            out.println("生年月日："+data[2]+"<br>");
+            out.println("住所："+data[3]+"<br>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
